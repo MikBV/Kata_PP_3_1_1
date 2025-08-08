be8096f83f9041c9f1e998eb05f2a9baf29449e7
MikBV <bmv00791@gmail.com>
2025-08-07 23:37:58 -0300

    Initial commit

---
+++ src/test/java/org/example/kata_pp_3_1_1/KataPp311ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kata_pp_3_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataPp311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
